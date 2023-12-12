e4eb851505072910dffdb510280d43122f575fa7
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2023-12-12 16:52:31 -0300

    GET by id

--- src/main/java/br/com/leandrokhalel/OdontoPro/controller/DentistController.java
+++ src/main/java/br/com/leandrokhalel/OdontoPro/controller/DentistController.java
@@ -1,17 +1,12 @@
 package br.com.leandrokhalel.OdontoPro.controller;
 
 import br.com.leandrokhalel.OdontoPro.api.DentistResponse;
-import br.com.leandrokhalel.OdontoPro.api.DentistaRequest;
-import br.com.leandrokhalel.OdontoPro.repository.DentistRepository;
 import br.com.leandrokhalel.OdontoPro.service.DentistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/dentists")
@@ -22,9 +17,13 @@ public class DentistController {
 
     @GetMapping
     public ResponseEntity<Page<DentistResponse>> findAll(Pageable pageable) {
-
         Page<DentistResponse> dentists = dentistService.findAll(pageable);
-
         return ResponseEntity.ok(dentists);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DentistResponse> getById(@PathVariable Long id) {
+        DentistResponse response = dentistService.getReferenceById(id);
+        return ResponseEntity.ok(response);
     }
 }
