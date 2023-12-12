78375186bd0536d2dda00005ad696b462abbf4ea
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2023-12-12 14:50:42 -0300

    First commit

---
+++ src/main/java/br/com/leandrokhalel/OdontoPro/OdontoProApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrokhalel.OdontoPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontoProApplication.class, args);
+	}
+
+}
